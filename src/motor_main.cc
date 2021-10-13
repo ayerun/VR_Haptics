5a341bdd4e3e10490e393a7c6c6c302dea2fb26f
Arun Kumar <arunkumar2021@u.northwestern.edu>
2021-10-13 16:32:32 -0500

    Able to control motor while rendering scene

--- src/motor_main.cc
+++ src/motor_main.cc
@@ -28,39 +28,7 @@ void ShowHelp() {
     Log::Write(Log::Level::Info, "Spaces:                   View, Local, Stage");
 }
 
-bool UpdateOptionsFromCommandLine(Options& options, int argc, char* argv[]) {
-    int i = 3;  // Index 0 is the program name and is skipped.
-
-    auto getNextArg = [&] {
-        if (i >= argc) {
-            throw std::invalid_argument("Argument parameter missing");
-        }
-
-        return std::string(argv[i++]);
-    };
-
-    while (i < argc) {
-        const std::string arg = getNextArg();
-        if (EqualsIgnoreCase(arg, "--graphics") || EqualsIgnoreCase(arg, "-g")) {
-            options.GraphicsPlugin = getNextArg();
-        } else if (EqualsIgnoreCase(arg, "--formfactor") || EqualsIgnoreCase(arg, "-ff")) {
-            options.FormFactor = getNextArg();
-        } else if (EqualsIgnoreCase(arg, "--viewconfig") || EqualsIgnoreCase(arg, "-vc")) {
-            options.ViewConfiguration = getNextArg();
-        } else if (EqualsIgnoreCase(arg, "--blendmode") || EqualsIgnoreCase(arg, "-bm")) {
-            options.EnvironmentBlendMode = getNextArg();
-        } else if (EqualsIgnoreCase(arg, "--space") || EqualsIgnoreCase(arg, "-s")) {
-            options.AppSpace = getNextArg();
-        } else if (EqualsIgnoreCase(arg, "--verbose") || EqualsIgnoreCase(arg, "-v")) {
-            Log::SetLevel(Log::Level::Verbose);
-        } else if (EqualsIgnoreCase(arg, "--help") || EqualsIgnoreCase(arg, "-h")) {
-            ShowHelp();
-            return false;
-        } else {
-            throw std::invalid_argument(Fmt("Unknown argument: %s", arg.c_str()));
-        }
-    }
-
+bool UpdateOptionsFromCommandLine(Options& options) {
     // Set graphics plugin to Vulkan2 by default
     if (options.GraphicsPlugin.empty()) {
         options.GraphicsPlugin = "Vulkan2";
@@ -110,13 +78,7 @@ int main(int argc, char* argv[]) {
     //prepare odrive for torque control
     board.setClosedLoopControl(0);
     board.setTorqueControlMode(0);
-    board.sendTorqueCommand(0,0.1);
-
-    // Parse command-line arguments into Options.
-    std::shared_ptr<Options> options = std::make_shared<Options>();
-    if (!UpdateOptionsFromCommandLine(*options, argc, argv)) {
-        return 1;
-    }
+    board.sendTorqueCommand(0,0);
 
     // Spawn a thread to wait for a keypress
     static bool quitKeyPressed = false;
@@ -127,39 +89,46 @@ int main(int argc, char* argv[]) {
     }};
     exitPollingThread.detach();
 
-    bool requestRestart = false;
-    do {
-        // Create platform-specific implementation.
-        std::shared_ptr<IPlatformPlugin> platformPlugin = CreatePlatformPlugin(options);
+    auto renderThread = std::thread{[] {
+        // Parse command-line arguments into Options.
+        std::shared_ptr<Options> options = std::make_shared<Options>();
+        UpdateOptionsFromCommandLine(*options);
+        bool requestRestart = false;
+        do {
+            // Create platform-specific implementation.
+            std::shared_ptr<IPlatformPlugin> platformPlugin = CreatePlatformPlugin(options);
 
-        // Create graphics API implementation.
-        std::shared_ptr<IGraphicsPlugin> graphicsPlugin = CreateGraphicsPlugin(options, platformPlugin);
+            // Create graphics API implementation.
+            std::shared_ptr<IGraphicsPlugin> graphicsPlugin = CreateGraphicsPlugin(options, platformPlugin);
 
-        // Initialize the OpenXR program.
-        std::shared_ptr<IOpenXrProgram> program = CreateOpenXrProgram(options, platformPlugin, graphicsPlugin);
+            // Initialize the OpenXR program.
+            std::shared_ptr<IOpenXrProgram> program = CreateOpenXrProgram(options, platformPlugin, graphicsPlugin);
 
-        program->CreateInstance();
-        program->InitializeSystem();
-        program->InitializeSession();
-        program->CreateSwapchains();
+            program->CreateInstance();
+            program->InitializeSystem();
+            program->InitializeSession();
+            program->CreateSwapchains();
 
-        while (!quitKeyPressed) {
-            bool exitRenderLoop = false;
-            program->PollEvents(&exitRenderLoop, &requestRestart);
-            if (exitRenderLoop) {
-                break;
+            while (!quitKeyPressed) {
+                bool exitRenderLoop = false;
+                program->PollEvents(&exitRenderLoop, &requestRestart);
+                if (exitRenderLoop) {
+                    break;
+                }
+
+                if (program->IsSessionRunning()) {
+                    program->PollActions();
+                    program->RenderFrame();
+                } else {
+                    // Throttle loop since xrWaitFrame won't be called.
+                    std::this_thread::sleep_for(std::chrono::milliseconds(250));
+                }
             }
 
-            if (program->IsSessionRunning()) {
-                program->PollActions();
-                program->RenderFrame();
-            } else {
-                // Throttle loop since xrWaitFrame won't be called.
-                std::this_thread::sleep_for(std::chrono::milliseconds(250));
-            }
-        }
-
-    } while (!quitKeyPressed && requestRestart);
+        } while (!quitKeyPressed && requestRestart);
+    }};
+    renderThread.detach();
+    
 
     //exit application
     return coreApplication.exec();
